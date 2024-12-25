50788613c6606c1d916a2b8783e9b2e3433da666
ikeeyigsys5575 <58853620+ikeeyigsys5575@users.noreply.github.com>
2024-12-24 23:35:45 -0500

    Initial Commit

---
+++ FireToDoTests/FireToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  FireToDoTests.swift
+//  FireToDoTests
+//
+//  Created by ikeeyigsys5575 on 12/24/24.
+//
+
+import Testing
+@testable import FireToDo
+
+struct FireToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
